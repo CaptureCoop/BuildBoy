b20116c6adcb726abd5cc0a45e1ac4642a5ec7aa
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2024-02-01 16:04:30 +0100

    build functionality

--- src/main/kotlin/org/capturecoop/buildboy/BuildBoyApplication.kt
+++ src/main/kotlin/org/capturecoop/buildboy/BuildBoyApplication.kt
@@ -1,15 +1,20 @@
 package org.capturecoop.buildboy
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.nio.file.Files
 
-class BuildBoyApplication() {
+class BuildBoyApplication {
     private val logger = LoggerFactory.getLogger("BuildBoy")
     private val rootFolder = File("BuildBoy").apply { mkdir() }
     private val reposFolder = File(rootFolder, "repos").apply { mkdir() }
     private val buildScriptsFolder = File(rootFolder, "buildscripts").apply { mkdir() }
+    private val buildsFolder = File(rootFolder, "builds").apply { mkdir() }
+
+    private val buildJsonWriter = Json { prettyPrint = true }
 
     private val buildScripts = buildScriptsFolder
         .listFiles()
@@ -25,28 +30,40 @@ class BuildBoyApplication() {
         buildScripts.forEach(::checkAndBuild)
     }
 
-    private fun checkAndBuild(buildScript: BuildScript) {
-        //val baseFolder = //Files.createTempDirectory("BuildRepo").toFile()
-        val repoFolder = File(reposFolder, buildScript.repoName).apply { mkdir() }
-        //if(!repoFolder.exists()) {
-            //repoFolder.mkdir()
-            val t = ExecUtils.exec("git status", repoFolder)
-            println(t)
-        //}
-        println(repoFolder)
+    @Serializable
+    data class BuildInfo(
+        val hash: String,
+        val author: String,
+        val timestamp: String,
+        val message: String,
+        val version: String
+    )
 
-        //val repoFolder = File(baseFolder, buildScript.repoName)
-        //val libsFolder = File(repoFolder, buildScript.buildDir)
-//
-        //ExecUtils.exec("git clone -b ${buildScript.branch} ${buildScript.repo}", baseFolder)
-        //val latest = ExecUtils.getLatestCommit(repoFolder)
-        //val version = File(repoFolder, "version.txt").readText()
-        //val name = "build-${latest.hash}-$version"
-        //println(name)
-        //println("Timestamp: $timestamp")
-        //buildScript.preBuildScripts.forEach { command -> ExecUtils.exec(command, repoFolder) }
-        //ExecUtils.exec(buildScript.buildScript, repoFolder)
-        //println("Successfully built: ${libsFolder.listFiles()?.map { it.name }}")
-        //Desktop.getDesktop().open(libsFolder)
+    private fun checkAndBuild(buildScript: BuildScript) {
+        val repoFolder = File(reposFolder, "${buildScript.repoName}-${buildScript.branch}/${buildScript.repoName}")
+        if(!repoFolder.exists()) {
+            repoFolder.mkdirs()
+            ExecUtils.exec("git clone -b ${buildScript.branch} ${buildScript.repo}", repoFolder.parentFile)
+        } else {
+            ExecUtils.exec("git pull", repoFolder)
+        }
+
+        val latest = ExecUtils.getLatestCommit(repoFolder)
+        val version = File(repoFolder, "version.txt").readText()
+        val buildFolder = File(buildsFolder, "${buildScript.repoName}/${buildScript.branch}/build-${latest.hash}-$version")
+        if(!buildFolder.exists() || (buildFolder.listFiles() ?: emptyArray()).isEmpty()) {
+            buildScript.preBuildScripts.forEach { command -> ExecUtils.exec(command, repoFolder) }
+            ExecUtils.exec(buildScript.buildScript, repoFolder)
+
+            File(repoFolder, buildScript.buildDir).copyRecursively(buildFolder)
+            val buildInfo = BuildInfo(
+                hash = latest.hash,
+                author = latest.author,
+                timestamp = latest.timestamp,
+                message = latest.subject,
+                version = version
+            )
+            File(buildFolder, "build.json").writeText(buildJsonWriter.encodeToString(buildInfo))
+        }
     }
 }
